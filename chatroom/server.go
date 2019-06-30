f3dc32e1ef818367fa238e6fa3110fd86a3c1727
Tony Zhang <nz97@cornell.edu>
2019-06-29 17:04:45 -0700

    master listener

--- chatroom/server.go
+++ chatroom/server.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 	"strconv"
 )
 
-// only needed below for sample processing
-
 type server struct {
 	physID     uint16
 	gridSize   uint16
+	masterIP   string
 	masterPort uint16
+	gridIP     string
+	shouldRun  bool
 }
 
+// newServer is the constructor for server.
+// It returns a server struct with default values for some fields.
+func newServer(physID uint16, gridSize uint16, masterPort uint16) server {
+	return server{
+		uint16(physID),
+		uint16(gridSize),
+		"127.0.0.1",
+		uint16(masterPort),
+		"127.0.0.1",
+		true}
+}
+
+// String is the "toString" method for a server object
+// It returns a string describing this server s.
 func (s server) String() string {
 	return fmt.Sprintf("* GoOvid server *\n"+
 		"physID: %d\n"+
@@ -29,35 +45,49 @@ func main() {
 	gridSize, err2 := strconv.ParseUint(os.Args[2], 10, 16)
 	masterPort, err3 := strconv.ParseUint(os.Args[3], 10, 16)
 	if err1 != nil || err2 != nil || err3 != nil {
-		errStr := fmt.Sprintf("Errors occured while processing arguments.\n"+
+		fmt.Printf("Errors occured while processing arguments.\n"+
 			"PhysID: %v\n"+
 			"gridSize: %v\n"+
 			"masterPort: %v\n"+
-			"Program exiting...",
+			"Program exiting...\n",
 			err1, err2, err3)
-		fmt.Println(errStr)
+		os.Exit(1)
+	}
+	if masterPort < 1024 {
+		fmt.Printf("Port number %d is a well-known port and cannot be used "+
+			"as masterPort\n", masterPort)
 		os.Exit(1)
 	}
 
-	server := server{uint16(pid), uint16(gridSize), uint16(masterPort)}
+	// initialize server
 	fmt.Println("Launching server...")
+	server := newServer(
+		uint16(pid),
+		uint16(gridSize),
+		uint16(masterPort))
 	fmt.Println(server)
 
-	// // listen on all interfaces
-	// ln, _ := net.Listen("tcp", "127.0.0.1:8081")
+	// listen for master on the master address
+	masterAddr := fmt.Sprintf("%s:%d", server.masterIP, server.masterPort)
+	fmt.Println("Listening for master connecting on " + masterAddr)
+	masterListener, _ := net.Listen("tcp", masterAddr)
+	masterConn, _ := masterListener.Accept()
+	fmt.Println(masterConn)
 
-	// // accept connection on port
-	// conn, _ := ln.Accept()
+	// for server.shouldRun {
+	// 		// accept connection on port
+	//
 
-	// // run loop forever (or until ctrl-c)
-	// for {
-	// 	// will listen for message to process ending in newline (\n)
-	// 	message, _ := bufio.NewReader(conn).ReadString('\n')
-	// 	// output message received
-	// 	fmt.Print("Message Received:", string(message))
-	// 	// sample process for string received
-	// 	newmessage := strings.ToUpper(message)
-	// 	// send new string back to client
-	// 	conn.Write([]byte(newmessage + "\n"))
+	// 		// run loop forever (or until ctrl-c)
+	// 		for {
+	// 			// will listen for message to process ending in newline (\n)
+	// 			message, _ := bufio.NewReader(conn).ReadString('\n')
+	// 			// output message received
+	// 			fmt.Print("Message Received:", string(message))
+	// 			// sample process for string received
+	// 			newmessage := strings.ToUpper(message)
+	// 			// send new string back to client
+	// 			conn.Write([]byte(newmessage + "\n"))
+	// 		}
 	// }
 }
