ec23d9cc088c14de707d572b83172a8981c4d648
Tony Zhang <nz97@cornell.edu>
2019-08-12 15:35:03 -0700

    update test to match new program

--- configs/parser_test.go
+++ configs/parser_test.go
@@ -3,8 +3,8 @@ package configs
 import (
 	"testing"
 
+	a "github.com/TonyZhangND/GoOvid/agents"
 	c "github.com/TonyZhangND/GoOvid/commons"
-	a "github.com/TonyZhangND/GoOvid/server/agents"
 )
 
 // Tests the correctness of the parser on chat.json
@@ -22,15 +22,18 @@ func TestParser_Chat(t *testing.T) {
 	for k, v := range agent.RawAttrs {
 		switch k {
 		case "myname":
-			if v.(string) != "client1" {
-				t.Errorf("agent 10 has name %v; want %s", v, "client1")
+			if v.(string) != "alice" {
+				t.Errorf("agent 10 has name %v; want %s", v, "alice")
 			}
 		case "contacts":
-			if len(v.([]interface{})) != 1 {
-				t.Errorf("agent 10 has contact %v; want %s", v, "[20]")
+			if len(v.([]interface{})) != 2 {
+				t.Errorf("agent 10 has contact %v; want %s", v, "[20, 30]")
 			}
 			if int(v.([]interface{})[0].(float64)) != 20 {
-				t.Errorf("agent 10 has contact %v; want %s", v, "[20]")
+				t.Errorf("agent 10 has contact %v; want %s", v, "[20, 30]")
+			}
+			if int(v.([]interface{})[1].(float64)) != 30 {
+				t.Errorf("agent 10 has contact %v; want %s", v, "[20, 30]")
 			}
 		default:
 			t.Errorf("agent 10 has invalid attr %s", k)
@@ -40,6 +43,10 @@ func TestParser_Chat(t *testing.T) {
 		switch int(vid) {
 		case 20:
 			if int(rt.DestID) != 20 || int(rt.DestPort) != 100 {
+				t.Errorf("agent 10 has invalid route %v, %v", vid, rt)
+			}
+		case 30:
+			if int(rt.DestID) != 30 || int(rt.DestPort) != 100 {
 				t.Errorf("agent 10 has invalid route %v, %v", vid, rt)
 			}
 		default:
@@ -52,15 +59,18 @@ func TestParser_Chat(t *testing.T) {
 	for k, v := range agent.RawAttrs {
 		switch k {
 		case "myname":
-			if v.(string) != "client2" {
-				t.Errorf("agent 20 has name %v; want %s", v, "client2")
+			if v.(string) != "bob" {
+				t.Errorf("agent 20 has name %v; want %s", v, "bob")
 			}
 		case "contacts":
-			if len(v.([]interface{})) != 1 {
-				t.Errorf("agent 20 has contact %v; want %s", v, "[10]")
+			if len(v.([]interface{})) != 2 {
+				t.Errorf("agent 20 has contact %v; want %s", v, "[10, 30]")
 			}
 			if int(v.([]interface{})[0].(float64)) != 10 {
-				t.Errorf("agent 20 has contact %v; want %s", v, "[10]")
+				t.Errorf("agent 20 has contact %v; want %s", v, "[10, 30]")
+			}
+			if int(v.([]interface{})[1].(float64)) != 30 {
+				t.Errorf("agent 20 has contact %v; want %s", v, "[10, 30]")
 			}
 		default:
 			t.Errorf("agent 20 has invalid attr %s", k)
