1f189490d1058930740196fd4973c98e56351cd8
Tony Zhang <nz97@cornell.edu>
2019-10-15 20:50:09 -0400

    draft paxos config and generator

---
+++ configs/paxos_generator.py
@@ -0,0 +1,12 @@
+"""
+Generates a Paxos configuration given the input parameters.
+Prints the generated configuration to standard output
+"""
+
+import sys
+
+def generate(f, num_clients, message_loss):
+
+
+if __name__ == '__main__':
+
