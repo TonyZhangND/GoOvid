a6067531794588c8640c27393d6a88c898072348
Tony Zhang <nz97@cornell.edu>
2019-10-26 17:43:39 -0400

    implement checker for log consistency

---
+++ checkConsistency.py
@@ -0,0 +1,24 @@
+# Checks dumps for consistency issues
+
+import glob
+
+
+outputFiles = glob.glob('tmp/replica*.output')
+
+# number of lines in the longest file
+max_lines =  max([sum(1 for line in open(f)) for f in outputFiles])
+
+file_lines = []
+for name in outputFiles:
+    f = open(name)
+    file_lines.append(f.readlines())
+
+for i in range(max_lines):
+    s = set()
+    for f in file_lines:
+        if i < len(f):
+            s.add(f[i])
+    if len(s) > 1:
+        print(f"Inconsistency detected in line {i+1}")
+        exit(1)
+print("All good :)")
