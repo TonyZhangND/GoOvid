fd885365dda2cf0e71ac738b4caf9b5186855a6b
Tony Zhang <nz97@cornell.edu>
2019-10-26 20:05:25 -0400

    improvements

--- checkConsistency.py
+++ checkConsistency.py
@@ -4,6 +4,7 @@ import glob
 
 
 outputFiles = glob.glob('tmp/replica*.output')
+outputFiles.sort()
 
 # number of lines in the longest file
 max_lines =  max([sum(1 for line in open(f)) for f in outputFiles])
@@ -20,5 +21,7 @@ for i in range(max_lines):
             s.add(f[i])
     if len(s) > 1:
         print(f"Inconsistency detected in line {i+1}")
+        lines = [fl[i].strip() for fl in file_lines]
+        print(",\n".join(lines))
         exit(1)
 print("All good :)")
